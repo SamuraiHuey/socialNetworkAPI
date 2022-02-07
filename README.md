57ed088fec18f3670989b957f91c4486efcd8aa9
Will B <tryarc@comcast.net>
2022-02-07 16:48:30 -0500

    repaired routing and added config

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # socialNetworkAPI
 
-An API for a social network to use for Posts and User creation
+An API for a social network to use for Posts and User creation. This API can be used by many different types of social networks to provide a high
 
 Repository: https://github.com/SamuraiHuey/techBlog.git
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const thoughtRoutes = require('/thoughts-routes');
-const userRoutes = require('/user-routes');
+const thoughtRoutes = require('./thoughts-routes');
+const userRoutes = require('./user-routes');
 
 router.use('/thoughts', thoughtRoutes);
 router.use('/users', userRoutes);
