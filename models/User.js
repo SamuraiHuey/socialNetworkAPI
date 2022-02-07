14b75336b589b79c955003cb6ffbbc3e497fc717
Will B <tryarc@comcast.net>
2022-02-06 21:26:32 -0500

    added models

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const Thought = require('./Thought');
+
+
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    },
+    thoughts: [],
+    friends: []
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User
